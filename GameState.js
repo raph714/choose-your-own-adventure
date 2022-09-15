22997081d21b9fe37d18ac53f216a2c88da00d91
Raphael DeFranco <raphael.defranco@gmail.com>
2022-09-15 12:35:16 -0700

    Initial commit.

---
+++ GameState.js
@@ -0,0 +1,43 @@
+const params = new URLSearchParams(window.location.search);
+
+const inventoryKey = {
+  hasShovel: "a",
+  hasHat: "b"
+};
+
+// Pass in a `inventoryKey` to see if it exists for the current game state.
+// eg if(valueForKey(inventoryKey.hasShovel)) { ... }
+function valueForKey(itemKey) {
+  return params.get(itemKey);
+}
+
+// Title will be the link title, href should be the name of the html page, without ext
+// inventoryUpdate should be a Map with inventoryKey keys and values. A `null` value
+// will remove the key from the query.
+//
+// const updates = new Map(); 
+// updates[inventoryKey.hasShovel] = 1; <- adding a shovel
+// updates[inventoryKey.hasHat] = null; <- removing a hat
+// addLink("Link Name", "htmlFileName", updates);
+//
+function addLink(title, href, inventoryUpdate) {
+  const paramsCopy = new URLSearchParams(params.toString());
+  for (key in inventoryUpdate) {
+    if (inventoryUpdate[key] == null) {
+      paramsCopy.delete(key);
+    } else {
+      paramsCopy.set(key, inventoryUpdate[key]);
+    }
+  }
+  var a = document.createElement("a");
+  var linkText = document.createTextNode(title);
+  a.appendChild(linkText);
+  a.title = title;
+  a.href = "./" + href + ".html" + "?" + paramsCopy.toString();
+
+  var links = document.getElementById("links");
+  links.appendChild(a);
+
+  var br = document.createElement("br");
+  links.appendChild(br);
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # choose-your-own-adventure
+
+Welcome to a simple, text based adventure.
+
+## Functions
+
+There are some basic inventory-like functions available to track state, amounts of items or any other minor bits of information. The state is stored in the URL as query parameters.
+
+See `GameState.js` for available functions and their usage.
